3f74d22a6008cc63b0242908a3587868f5faa26d
Richard You <richardyou94@gmail.com>
2022-09-30 09:07:00 -0700

    add get and post request of notes

--- server.js
+++ server.js
@@ -1,17 +1,57 @@
 const express = require('express');
 const path = require('path');
+const noteData = require('./db/db.json')
 
 const app = express();
-const noteData = require('./db/db.json')
+
+
 const PORT = 3001;
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());    
+
 app.use(express.static('public'));
+
+// GET request 
 
 app.get('/', (req, res) => res.send('send'));
 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to get note`);
+    return res.status(200).json(noteData);
+
+});
+
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+// POST request
+app.post('/api/notes', (req, res) => {
+
+    console.info(req.rawHeaders);
+
+    console.info(`${req.method} request received`);
+
+    let response;
+
+    if(req.body && req.body.title) {
+        response = {
+            status: 'success',
+            data: req.body,
+        };
+        res.status(201).json(response);
+    } else {
+        res.status(400).json('Request body must at least contain a title of the note');
+    }
+
+    console.log(req.body);
+})
 
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
